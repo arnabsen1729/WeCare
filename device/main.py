eac017e90c162d42a9f0511e5d020a988799b233
Mr-DareDevil <arnabsen1729@gmail.com>
2021-02-12 21:52:41 +0530

    Added device request feature

--- device/main.py
+++ device/main.py
@@ -1,8 +1,10 @@
 import serial
 from time import sleep
+import requests
 
 ARD_PORT = "/dev/ttyUSB0"
-
+DEVICE_ID = 1234
+URL = "http://localhost:3000/device"
 
 si = serial.Serial(ARD_PORT,baudrate=9600,timeout=1)
 
@@ -10,18 +12,19 @@ def parseData(rawData):
   rawDataStripped = rawData.strip()
   finalData = rawDataStripped.split("@")
   parsedData = {}
-  parsedData["temp"]=finalData[0]
-  parsedData["humi"]=finalData[1]
+  parsedData["device_id"]=DEVICE_ID
+  parsedData["temperature"]=finalData[0]
+  parsedData["humidity"]=finalData[1]
   parsedData["pressure"]=finalData[2]
-  parsedData["alt"]=finalData[3]
+  parsedData["altitude"]=finalData[3]
   return parsedData
-
 
 def main():
   while True:
     data = si.readline().decode('ascii')
     if data.rstrip()!="":
       finalData = parseData(data)
+      requests.post(url=URL, data=finalData)
       print(finalData)
 
 
